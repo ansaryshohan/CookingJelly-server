f2c189fa2ad08b8ca637541558d012fbcf98b61e
ansaryshohan <ansarye2083@gmaail.com>
2023-01-05 08:05:54 +0600

    mongodb is connected to the user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port=process.env.PORT||5000;
 
@@ -7,6 +8,18 @@ const app= express();
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.evsael1.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/',(req,res)=>{
   res.send({message:'server is running'})
