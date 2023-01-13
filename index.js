d66692c56a67a5bf96c175c4098ca77409ba94f3
ansaryshohan <ansarye2083@gmaail.com>
2023-01-14 03:49:58 +0600

    modified

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ async function run() {
     })
 
     app.get('/singleProduct/:id', async (req, res) => {
-      const id = req.params.id
-      const query = { _id: ObjectId(id)}
+      const id = parseInt(req.params.id)
+      const query = { product_id: { $eq: id } }
       const singleProduct = await allSingleProductDB.findOne(query);
       res.send({ message: 'true', data: singleProduct });
     })
